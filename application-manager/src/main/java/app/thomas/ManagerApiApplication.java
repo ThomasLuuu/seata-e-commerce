d31d0ab65240e67f68f2af60150bd96452ec2c13
thomas.tld <thomas.tld@byteuptech.com>
2025-09-17 14:46:47 +0700

    new for zoo

--- application-manager/src/main/java/app/thomas/ManagerApiApplication.java
+++ application-manager/src/main/java/app/thomas/ManagerApiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ManagerApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ManagerApiApplication.class, args);
 	}
